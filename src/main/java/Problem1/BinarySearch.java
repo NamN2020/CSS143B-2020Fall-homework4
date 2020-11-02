d636d613c73db041eeb196c9a323beb59fa9e79a
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-02 15:16:29 -0800

    BinarySearchRecursively Complete

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -9,8 +9,27 @@ public class BinarySearch {
     // This is to be done recursively
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
+    public static int binarySearch(int[] data, int target, int lhs, int rhs) {
+        //  base case if target is not found
+        if(lhs > rhs){
+            return -1;
+        }
+        //  lhs + (rhs - lhs) / 2 to avoid OverFlowException
+        int mid = lhs + (rhs - lhs) / 2;
+
+        if(target > data[mid]){
+            return binarySearch(data, target, mid + 1, rhs);
+        } else if(target < data[mid]){
+            return binarySearch(data, target, lhs, mid - 1);
+        } else if(target == data[mid]){
+            return mid;
+        }
+
+        return -1;
+    }
+
     public static int binarySearch(int[] data, int target) {
         // homework
-        return -1;  // place holder
+        return binarySearch(data, target, 0, data.length - 1);
     }
 }
