21e31503c64b101a5dba8e2f9b5dd5a49a65264f
NamN2020 <72000121+NamN2020@users.noreply.github.com>
2020-11-03 09:26:10 -0800

    ReverseStack Recursively Complete

--- src/main/java/Problem1/ReverseStack.java
+++ src/main/java/Problem1/ReverseStack.java
@@ -5,10 +5,35 @@ package Problem1;
     CSS 143B
     Homework 4
  */
+
 import java.util.Stack;
 
 public class ReverseStack {
+    public static void bottomFirst(Stack<Integer> stack, int bottom) {
+        // base case
+        if (stack.isEmpty()) {
+            stack.push(bottom);
+            return;
+        }
+        // keep popping stack until it is empty, than add the bottom first
+        int top = stack.pop();
+        bottomFirst(stack, bottom);
+
+        // add back all the top values in correct order
+        stack.push(top);
+    }
+
     public static void reverseStack(Stack<Integer> stack) {
-        // homework
+        // base case
+        if(stack.isEmpty()){
+            return;
+        }
+
+        // keep popping stack until empty to find the very bottom
+        int bottom = stack.pop();
+        reverseStack(stack);
+
+        // first item popped is last item in new stack
+        bottomFirst(stack, bottom);
     }
 }
